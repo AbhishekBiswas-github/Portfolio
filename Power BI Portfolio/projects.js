b6412a0e7c38433a365e4da1947e0183b484125a
Abhishek Biswas <67058151+AbhishekBiswas-github@users.noreply.github.com>
2025-08-12 14:15:50 +0530

    Add files via upload

---
+++ Power BI Portfolio/projects.js
@@ -0,0 +1,64 @@
+export const projects = [
+  {
+    title: "US Flight Report",
+    image: "images/Plane.jpg",
+    date: "November 28, 2024",
+    tools: [],
+    repo: "https://github.com/AbhishekBiswas-github/Microsoft_PowerBI_Projects/tree/main/us_flights_report",
+  },
+  {
+    title: "Student Performance Report",
+    image: "images/Student.jpg",
+    date: "August 10, 2024",
+    tools: ["Pandas", "NumPy", "Matplotlib", "Seaborn", "Plotly"],
+    repo: "https://github.com/AbhishekBiswas-github/Microsoft_PowerBI_Projects/tree/main/student_performance_report",
+  },
+  {
+    title: "Shark Tank Report",
+    image: "images/Investors.jpg",
+    date: "August 12, 2024",
+    tools: ["Logistic Regression", "Decision Tree", "Random Forest"],
+    repo: "https://github.com/AbhishekBiswas-github/Microsoft_PowerBI_Projects/tree/main/shark_tank_report",
+  },
+  {
+    title: "Microsoft Stock Report",
+    image: "images/MS_Stock.jpg",
+    date: "December 3, 2024",
+    tools: [
+      "Pandas",
+      "Matplotlib & Seaborn",
+      "Logistic Regression",
+      "Decision Tree",
+      "Random Forest",
+    ],
+    repo: "https://github.com/AbhishekBiswas-github/Microsoft_PowerBI_Projects/tree/main/Microsoft%20Stock%20Report",
+  },
+  {
+    title: "MBA Admission Report",
+    image: "images/MBA.jpg",
+    date: "November 8, 2024",
+    tools: [
+      "Pandas",
+      "NumPy",
+      "Matplotlib & Seaborn",
+      "KMeans",
+      "OrdinalEncoder",
+      "StandardScaler",
+    ],
+    repo: "https://github.com/AbhishekBiswas-github/Microsoft_PowerBI_Projects/tree/main/MBA%20Admission%20Report",
+  },
+  {
+    title: "England Road Accident Report",
+    image: "images/Accident.jpg",
+    date: "July 28, 2025",
+    tools: [
+      "Pandas",
+      "NumPy",
+      "Matplotlib & Seaborn",
+      "Linear Regression",
+      "Random Forest Regression",
+    ],
+    repo: "https://github.com/AbhishekBiswas-github/Microsoft_PowerBI_Projects/tree/main/England%20Road%20Accident%20Report",
+  },
+  // Add more projects as needed
+];
